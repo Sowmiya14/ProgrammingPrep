1ef960373ebe1906c6a5a69ab6ca9a3e61c432d7
Sowmiya.K <34268334+Sowmiya14@users.noreply.github.com>
2024-01-16 16:35:15 +0530

    Add files via upload

--- Python/Ebbillcorrection.py
+++ Python/Ebbillcorrection.py
@@ -9,11 +9,11 @@ else:
     elif consumed_units <= 500: 
         cost_of_usage = 100*2.25 + 200*4.50 + (consumed_units - 400) * 6.00
     elif consumed_units <= 600:
-        cost_of_usage = 300*4.50 + 100*6.00 + (consumed_units - 500) * 8.00
+        cost_of_usage = 100*2.25 + 200*4.50 + 100*6.00 + (consumed_units - 500) * 8.00
     elif consumed_units <= 800:
-        cost_of_usage = 300*4.50 + 100*6.00 + 200*8.00 + (consumed_units - 600) * 9.00
+        cost_of_usage = 100*2.25 + 200*4.50 + 100*6.00 + 200*8.00 + (consumed_units - 600) * 9.00
     elif consumed_units <= 1000:
-        cost_of_usage = 300*4.50 + 100*6.00 + 200*8.00 + 200*9.00 + (consumed_units - 800) * 10.00
+        cost_of_usage = 100*2.25 + 200*4.50 + 100*6.00 + 200*8.00 + 200*9.00 + (consumed_units - 800) * 10.00
     else:
-        cost_of_usage = 300*4.50 + 100*6.00 + 200*8.00 + 200*9.00 + 200*10 + (consumed_units - 200) * 11.00
+        cost_of_usage = 100*2.25 + 200*4.50 + 100*6.00 + 200*8.00 + 200*9.00 + 200*10 + (consumed_units - 200) * 11.00
     print(f"The Electricity Bill is Rs : {cost_of_usage:.2f}.")
