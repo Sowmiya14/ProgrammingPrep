752dc68a2d09a5e67521781807e87ec5166c249e
Sowmiya.K <34268334+Sowmiya14@users.noreply.github.com>
2024-01-16 15:50:50 +0530

    Update EBbillcalculator.py

--- Python/EBbillcalculator.py
+++ Python/EBbillcalculator.py
@@ -1,7 +1,6 @@
 consumed_units = int(input("Enter the number of units consumed: "))
 
 if(consumed_units <= 500):
-    # print("sowmi")
     if consumed_units <= 100:
         print("The Electricity Bill is Free.")
     else:
@@ -14,19 +13,16 @@ if(consumed_units <= 500):
         print(f"The Electricity Bill is Rs : {cost_of_usage:.2f}.")
 
 else:
-    if consumed_units <= 100:
-        print("The Electricity Bill is Free.")
+    if consumed_units <= 400:
+        cost_of_usage = (consumed_units - 100) * 4.50
+    elif consumed_units <= 500: 
+        cost_of_usage = 300*4.50 + (consumed_units - 100) * 6.00
+    elif consumed_units <= 600:
+        cost_of_usage = 300*4.45 + 100*6.00 + (consumed_units - 100) * 8.00
+    elif consumed_units <= 800:
+        cost_of_usage = 300*4.45 + 100*6.00 + 200*8.00 + (consumed_units - 200) * 9.00
+    elif consumed_units <= 1000:
+        cost_of_usage = 300*4.45 + 100*6.00 + 200*8.00 + 200*9.00 + (consumed_units - 200) * 10.00
     else:
-        if consumed_units <= 400:
-            cost_of_usage = (consumed_units - 100) * 4.50
-        elif consumed_units <= 500: 
-            cost_of_usage = 300*4.50 + (consumed_units - 100) * 6.00
-        elif consumed_units <= 600:
-            cost_of_usage = 300*4.45 + 100*6.00 + (consumed_units - 100) * 8.00
-        elif consumed_units <= 800:
-            cost_of_usage = 300*4.45 + 100*6.00 + 200*8.00 + (consumed_units - 200) * 9.00
-        elif consumed_units <= 1000:
-            cost_of_usage = 300*4.45 + 100*6.00 + 200*8.00 + 200*9.00 + (consumed_units - 200) * 10.00
-        else:
-            cost_of_usage = 300*4.45 + 100*6.00 + 200*8.00 + 200*9.00 + 200*10 + (consumed_units - 200) * 11.00
-        print(f"The Electricity Bill is Rs : {cost_of_usage:.2f}.")
+        cost_of_usage = 300*4.45 + 100*6.00 + 200*8.00 + 200*9.00 + 200*10 + (consumed_units - 200) * 11.00
+    print(f"The Electricity Bill is Rs : {cost_of_usage:.2f}.")
